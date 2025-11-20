fe8aafd0f2fef36d8c0a51a25c3f2787087b2466
administracion secretaria de empleo <adm.secretariadeempleolr@gmail.com>
2025-11-20 21:15:44 +0000

    const firebaseConfig = {   apiKey: "AIzaSyCpj3Wr4OLepz8M333F5pJytAaDViw0

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-9944772026-932c4",
-  "appId": "1:61368342960:web:22e9bb8aec4587c6475e75",
-  "apiKey": "AIzaSyDUFX0vKr9_b6CTVQMw9o0ZjFiwUQWp7_4",
-  "authDomain": "studio-9944772026-932c4.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "61368342960"
+  apiKey: "AIzaSyCpj3Wr4OLepz8M333F5pJytAaDViw0YLk",
+  authDomain: "programas-spe.firebaseapp.com",
+  projectId: "programas-spe",
+  storageBucket: "programas-spe.appspot.com",
+  messagingSenderId: "1083251126",
+  appId: "1:1083251126:web:d1660b9524c30db29ec917",
+  measurementId: "G-9C2YFCK2L4"
 };
