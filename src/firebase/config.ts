5500ba272f95db54367ffec4e6d46f32ad3fc5f3
administracion secretaria de empleo <adm.secretariadeempleolr@gmail.com>
2025-11-20 16:24:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9944772026-932c4",
+  "appId": "1:61368342960:web:22e9bb8aec4587c6475e75",
+  "apiKey": "AIzaSyDUFX0vKr9_b6CTVQMw9o0ZjFiwUQWp7_4",
+  "authDomain": "studio-9944772026-932c4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "61368342960"
+};
